0240ca8e26b8c7856a36a698d6af280e8954739c
rossfergs <91530725+rossfergs@users.noreply.github.com>
2022-03-24 15:42:04 +0000

    Added more getters and an add method

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -33,19 +33,47 @@ public class TreeNode {
 
     /**
      * method used to set a new value to the leftNode field
-     * @param newWord string to set as the new word in the left treeNode
+     * @param newNode npde to set as the new node in the left treeNode
      */
-    public void setLeftNode(String newWord){
-        leftNode = new TreeNode(newWord);
+    public void setLeftNode(TreeNode newNode){
+        leftNode = newNode;
     }
+
 
     /**
      *  method used to set a new value to the rightNode field
-     *  @param newWord string to set as the new word in the left treeNode
+     *  @param newNode string to set as the new node in the left treeNode
      */
-    public void setRightNode(String newWord){
-        rightNode = new TreeNode(newWord);
+    public void setRightNode(TreeNode newNode){
+        rightNode = newNode;
     }
+
+    /**
+     * method to return the an index of intArray
+     * @return returns the interger stored in the desired index of the array
+     */
+    public int getArrayIndex(int i) {
+        return(intArray[i]);
+    }
+
+    /**
+     * method used to return the value stored in field LeftNode
+     * @return the value of leftNode
+     */
+    public TreeNode getLeftNode() {
+        return leftNode;
+    }
+
+    /**
+     * method used to return the value stored in field rightNode
+     * @return the value of rightNode
+     */
+    public TreeNode getRightNode() {
+        return rightNode;
+    }
+
+    public String getWord(){ return word; }
+
 
     /**
      * method used to convert a word into an array of integers, used for navigation within the binary tree
@@ -62,5 +90,24 @@ public class TreeNode {
         return(intArray);
     }
 
+    /**
+     * 
+     * @param newWord
+     */
+    public void addNode(String newWord) {
+        int i = 0;
+        boolean loop = true;
+        TreeNode newNode = new TreeNode(newWord);
+        //checking if the id is lower, higher or match, and placing the new node dependant on the result
 
+        if ((newNode.getArrayIndex(i) < this.getArrayIndex(i)) && (this.getLeftNode() == null)){
+            this.setLeftNode(newNode);
+        }
+        else if ((newNode.getArrayIndex(i) > this.getArrayIndex(i)) && (this.getRightNode() == null)) {
+            this.setRightNode(newNode);
+        }
+        else {
+            System.out.println("Error");
+        }
+    }
 }
